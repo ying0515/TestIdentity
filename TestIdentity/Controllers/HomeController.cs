7cdcd0feaa5eec927228f62f45b9215a144f335b
Tony Huang <ying515.huang@msa.hinet.net>
2017-04-18 22:45:19 +0800

    commit all

--- TestIdentity/Controllers/HomeController.cs
+++ TestIdentity/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace TestIdentity.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index()  
         {
             return View();
         }
